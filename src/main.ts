4e7d14b119849ae03eb56cf6f4cbbb575d72af81
Jaal-moha <mohammed79462h@gmail.com>
2025-10-02 17:41:26 +0300

    feat: enhance CORS configuration to support dynamic allowed origins

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,23 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module'; import { ValidationPipe } from '@nestjs/common';
 import { join } from 'node:path';
 import { existsSync, mkdirSync } from 'node:fs';
 
 
 async function bootstrap() {
   const uploadPath = join(__dirname, '/upload/staff-ids');
-  existsSync(uploadPath) || mkdirSync(uploadPath,{ recursive: true });
+  existsSync(uploadPath) || mkdirSync(uploadPath, { recursive: true });
   const app = await NestFactory.create(AppModule);
-  
+  const configService = app.get(ConfigService);
+
   // Enable CORS for frontend communication
   app.enableCors({
-    origin: 'http://localhost:3001', // frontend address
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: [
+      'http://localhost:3001', // frontend address
+      ...(JSON.parse(configService.get('ALLOWED_ORIGINS') ?? '[]') ?? []),
+    ],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
